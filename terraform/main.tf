9fe97e9d9bf52f316a9322c673b00e1185bb08ca
asim <asim9956ahm@gmail.com>
2025-11-09 16:51:50 +0530

    seventh

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "fastapi_server" {
+  ami           = "ami-02b8269d5e85954ef" # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = "newkp"
+
+  tags = {
+    Name = "FastAPI-Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.fastapi_server.public_ip
+}
